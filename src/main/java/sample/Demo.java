2571244cafd34ce97e621ef71ab6cef1ae892ba6
Paul Krause <pkrause@investsoftech.com>
2013-11-12 15:15:23 -0500

    Optimize whitepace for GitHib display

--- src/main/java/sample/Demo.java
+++ src/main/java/sample/Demo.java
@@ -7,34 +7,34 @@ import java.io.IOException;
 import server.BpCalcMakeCalc;
 
 public class Demo implements Runnable {
-	private static final String DLL_NAME = "BpDLL.dll";
-	private static final File BP_DIR = new File("C:\\BondProbe\\bin");
+    private static final String DLL_NAME = "BpDLL.dll";
+    private static final File BP_DIR = new File("C:\\BondProbe\\bin");
 
-	private final String[] bpData = {
-		"CALCTYPE=112=SECURITYID=325=SECTYPE=SBAM-SEMI-30/360=RV=103.875=SETTDT=10/15/2013=COUPON=7.75="
-		+ "PAR=1000000.0=ODD1DT=6/15/2010=ISSDT=6/11/2010=MATDT=10/18/2013",
-		"CALCTYPE=112=SECURITYID=1288=SECTYPE=US CORP=RV=102.0=SETTDT=10/15/2013=COUPON=0.0="
-		+ "PAR=1000000.0=ODD1DT=6/19/2007=ISSDT=12/19/2006=MATDT=10/18/2013",
-		"CALCTYPE=112=SECURITYID=1302=SECTYPE=US CORP=RV=101.5=SETTDT=10/15/2013=COUPON=8.5="
-		+ "PAR=1000000.0=ODD1DT=4/15/2013=ISSDT=10/10/2012=MATDT=10/20/2013",
-	};
+    private final String[] bpData = {
+            "CALCTYPE=112=SECURITYID=325=SECTYPE=SBAM-SEMI-30/360=RV=103.875=SETTDT=10/15/2013=COUPON=7.75="
+                    + "PAR=1000000.0=ODD1DT=6/15/2010=ISSDT=6/11/2010=MATDT=10/18/2013",
+            "CALCTYPE=112=SECURITYID=1288=SECTYPE=US CORP=RV=102.0=SETTDT=10/15/2013=COUPON=0.0="
+                    + "PAR=1000000.0=ODD1DT=6/19/2007=ISSDT=12/19/2006=MATDT=10/18/2013",
+            "CALCTYPE=112=SECURITYID=1302=SECTYPE=US CORP=RV=101.5=SETTDT=10/15/2013=COUPON=8.5="
+                    + "PAR=1000000.0=ODD1DT=4/15/2013=ISSDT=10/10/2012=MATDT=10/20/2013", };
 
-	@Override
-	public void run() {
-		try (BpCalcMakeCalc bp = new BpCalcMakeCalc(BP_DIR)) {
-			for (String bpin : bpData) {
-				String bpout = bp.calculate(bpin);
-				System.out.println(bpout);
-			}
-		}
-	}
+    @Override
+    public void run() {
+        try (BpCalcMakeCalc bp = new BpCalcMakeCalc(BP_DIR)) {
+            for (String bpin : bpData) {
+                String bpout = bp.calculate(bpin);
+                System.out.println(bpout);
+            }
+        }
+    }
 
-	public static void main(String[] args) throws IOException {
-		final File dll = new File(BP_DIR, DLL_NAME);
-		if (!dll.exists()) throw new FileNotFoundException(dll.toString());
-		System.load(dll.getAbsolutePath());
-		Demo demo = new Demo();
-		demo.run();
-	}
+    public static void main(String[] args) throws IOException {
+        final File dll = new File(BP_DIR, DLL_NAME);
+        if (!dll.exists())
+            throw new FileNotFoundException(dll.toString());
+        System.load(dll.getAbsolutePath());
+        Demo demo = new Demo();
+        demo.run();
+    }
 
 }
--- src/main/java/server/BpCalcMakeCalc.java
+++ src/main/java/server/BpCalcMakeCalc.java
@@ -1,43 +1,45 @@
 package server;
 
-
-import static java.util.logging.Logger.getLogger;
 import static java.util.logging.Level.INFO;
+import static java.util.logging.Logger.getLogger;
 
 import java.io.Closeable;
 import java.io.File;
 import java.util.logging.Logger;
 
-
 public class BpCalcMakeCalc implements Closeable {
 
     private static final Logger LOG = getLogger(BpCalcMakeCalc.class.getName());
+
     synchronized private native void bpStringInitialize(String bpdirectory);
+
     synchronized private native String bpStringCalculate(String ignore, String input);
+
     synchronized private native void bpStringClose();
 
-    private String path;
+    private final String path;
+
     public BpCalcMakeCalc(File dir) {
-    	LOG.log(INFO, "bpStringInitialize(\"{0}\")", dir);
-    	path = dir.getAbsolutePath();
-    	bpStringInitialize(path);
+        LOG.log(INFO, "bpStringInitialize(\"{0}\")", dir);
+        path = dir.getAbsolutePath();
+        bpStringInitialize(path);
     }
 
     public void reset() {
-    	LOG.log(INFO, "closing dll....");
+        LOG.log(INFO, "closing dll....");
         bpStringClose();
-    	LOG.log(INFO, "reInitializing dll path: {0}", path);
-    	bpStringInitialize(path);
+        LOG.log(INFO, "reInitializing dll path: {0}", path);
+        bpStringInitialize(path);
     }
 
     public String calculate(String input) {
-    	LOG.log(INFO, "bpStringCalculate(\"\", \"{0}\")", input);
-    	return bpStringCalculate("", input);
+        LOG.log(INFO, "bpStringCalculate(\"\", \"{0}\")", input);
+        return bpStringCalculate("", input);
     }
 
     @Override
     public void close() {
-    	LOG.info("bpStringClose()");
+        LOG.info("bpStringClose()");
         bpStringClose();
     }
 }
