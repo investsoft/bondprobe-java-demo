5d1df6be3e0bc1046c3e340f3884bb9e787299ca
Paul Krause <pkrause@investsoftech.com>
2013-11-12 14:16:50 -0500

    initial upload
    2 java files and 1 build file

---
+++ README.md
@@ -0,0 +1,6 @@
+This is a BondProbe integration demonstrator.  To build, you must install:
+
+1. BondProbe from InvestSoft Technology
+2. Java JDK 7 from Oracle Corporation
+3. Maven from Apache Software Foundation
+
---
+++ src/main/java/server/BpCalcMakeCalc.java
@@ -0,0 +1,43 @@
+package server;
+
+
+import static java.util.logging.Logger.getLogger;
+import static java.util.logging.Level.INFO;
+
+import java.io.Closeable;
+import java.io.File;
+import java.util.logging.Logger;
+
+
+public class BpCalcMakeCalc implements Closeable {
+
+    private static final Logger LOG = getLogger(BpCalcMakeCalc.class.getName());
+	synchronized private native void bpStringInitialize(String bpdirectory);
+    synchronized private native String bpStringCalculate(String ignore, String input);
+    synchronized private native void bpStringClose();
+
+    private String path;
+    public BpCalcMakeCalc(File dir) {
+    	LOG.log(INFO, "bpStringInitialize(\"{0}\")", dir);
+    	path = dir.getAbsolutePath();
+    	bpStringInitialize(path);
+    }
+
+    public void reset() {
+    	LOG.log(INFO, "closing dll....");
+		bpStringClose();
+    	LOG.log(INFO, "reInitializing dll path: {0}", path);
+    	bpStringInitialize(path);
+    }
+    
+    public String calculate(String input) {
+    	LOG.log(INFO, "bpStringCalculate(\"\", \"{0}\")", input);
+    	return bpStringCalculate("", input);
+    }
+    
+	@Override
+	public void close() {
+    	LOG.info("bpStringClose()");
+		bpStringClose();
+	}
+}
